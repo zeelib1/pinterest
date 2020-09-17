642e3c85e7c9629e1af4fabbbafc8d5d7f31e1ee
Joschka Cesar <cesar@posteo.de>
2020-09-17 14:12:04 +0200

    fix fetch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
 import Nav from './Components/Nav';
 import Profile from './Components/Profile';
 import Chart from './Components/Chart';
 import cinema from '../src/img/cinema.svg';
 import { useFetch } from './Components/Hooks';
-
-// Link to asset
-// https://cdn.contentful.com/spaces/3jl15wjpl83d/environments/master/assets/72UF6pHNFstlykyUHBr1M6?access_token=00cu3XCQpVWs8x8LReew9HvhN2G9ktMpgDHnpN574xA
 
 function App() {
 
@@ -35,21 +31,17 @@ function Home (){
     fetch('https://cdn.contentful.com/spaces/3jl15wjpl83d/environments/master/entries?access_token=00cu3XCQpVWs8x8LReew9HvhN2G9ktMpgDHnpN574xA&content_type=post')
       .then((value) => value.json())
       .then((json) => json)
-      .then((result) => result.items.map((element) => {
-        console.log(element.fields.image.sys.id);
-        console.log(result.includes);
-        
-      }))
+      .then((result) => {result.items.map((element) => {
+        result.includes.Asset.forEach(asset => {
+          if (asset.sys.id === element.fields.image.sys.id) {
+            element.imgUrl = asset.fields.file.url.slice(2);
+            return element;
+          }
+        });
+      })
+      setMovies(result);
+      })
       .catch((e) => e)
-
- /*      movies.items.map((element) => {
-          fetch(`https://cdn.contentful.com/spaces/3jl15wjpl83d/environments/master/assets/${element.fields.image.sys.id}?access_token=00cu3XCQpVWs8x8LReew9HvhN2G9ktMpgDHnpN574xA`)
-            .then((value) => value.json())
-            .then((json) => {
-              element.imgurl = json.fields.file.url.slice(2);
-            })
-            .catch((e) => e)
-      })  */
   },[])
     
 
@@ -61,14 +53,13 @@ function Home (){
          <button className="submitButton" type="submit">Submit</button>
          </form>
          <div className="gridContainer">
-        {/*  {movies ? 
+         {movies ? 
              movies.items.map((element) => {
-               console.log(element);
               return (<div className="photoOne" key={element.sys.id}>
                 <img className="grow"  
-                src={`http://images.ctfassets.net/3jl15wjpl83d/wMsK4RN8ITvve0RJOGBRQ/67ce855894e42d23ec0be9ee11cc5fe3/iyTD2QnySNMPUPE3IedZQipSWfz.jpg`} 
+                src={`http://${element.imgUrl}`} 
                 alt={element.fields.name} />
-                </div>);}) */}
+                </div>);}) 
             : null} 
         </div>
       </div>
